26ff2c6601263f52eb865ff9eac7193930952fec
emerlon3 <e.magdua@mlgcl.edu.ph>
2025-02-17 17:23:36 +0800

    2-17-2025 activity 1

---
+++ database/factories/employeeFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\employees>
+ */
+class employeeFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'first_name' => $this->faker->firstName(),
+            'last_name' => $this->faker->lastName(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'position' => $this->faker->jobTitle(),
+            'salary' => $this->faker->numberBetween(30000, 120000),
+          
+        ];
+    }
+}
