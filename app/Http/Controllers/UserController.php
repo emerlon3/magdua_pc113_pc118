c373647a25b8cf9c6b4de4f4c4fc1d18934e8beb
emerlon3 <e.magdua@mlgcl.edu.ph>
2025-03-22 14:57:51 +0800

    Activity 5 try and catch 23/03/2025

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,101 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Validation\ValidationException;
+use Exception;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    //
+    public function show()
+    {
+        return response ()->json(User::all());
+ 
+   }
+
+ 
+   public function create(Request $request){
+    try {
+        $validate = $request->validate([
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|max:255',
+            'email' => 'required|string|email|max:255|unique:students',
+            'course' => 'required|max:255',
+            'password' => 'required',
+            'role' => 'required',
+        ]);
+
+        $user = User::create([
+            'first_name' => $validate['first_name'],
+            'last_name' => $validate['last_name'],
+            'email' => $validate['email'],
+            'course' => $validate['course'],
+            'password' => bcrypt($validate['password']), // Encrypt password
+            'role' => $validate['role'],
+        ]);
+
+        return response()->json([
+            'message' => 'User registration successful',
+            'user' => $user
+        ], 201);
+    } catch (ValidationException $e) {
+        return response()->json(['error' => $e->errors()], 422);
+    } catch (Exception $e) {
+        return response()->json(['error' => 'Something went wrong'], 500);
+    }
+}
+
+public function login(Request $request){
+    try {
+        $student = User::where('email', $request->email)->first();
+        
+        if ($student && Hash::check($request->password, $student->password)) {
+            $token = $student->createToken('auth_token')->plainTextToken;
+            
+            return response()->json([
+                'message' => 'Login successful',
+                'token' => $token,
+                'student' => $student
+            ], 200);
+        }
+        
+        return response()->json(['message' => 'Login failed'], 401);
+    } catch (Exception $e) {
+        return response()->json(['error' => 'Something went wrong'], 500);
+    }
+}
+
+
+          public function update (Request $request, $id)
+          {
+              $users = User::find($id);
+                  if (is_null($users)) {
+                      return response()->json(['message' => 'Employee not found'], 404);
+                  }
+      
+                  $validatedData = $request->validate([
+                      'first_name' => 'string', 'last_name' => 'string', 'email' =>'string', 'course' => 'string',  'role' => 'string']);
+                  $users->update($validatedData);
+                  return response()->json([
+                    'message' => 'Employee updated successfully', 
+                    'user' => $users
+      
+                    
+                  ]);
+              }
+       
+              public function delete($id)
+              {
+               $users = User:: find($id);
+              if (is_null($users)) {
+              return response()->json(['message' => 'Users not found'], 404);
+              }
+              $users->delete();
+              return response()->json([
+                  
+              'message' => 'Users deleted successfully',
+                  
+              ]);
+              }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ListController;
-use App\Http\Controllers\AuthController;
+
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,25 +22,38 @@ use App\Http\Controllers\AuthController;
  
 
 
-Route::post('/student-login', [AuthController::class, 'login']);
-Route::post('/registration', [AuthController::class, 'create']);
+// Route::post('/student-login', [AuthController::class, 'login']);
+// Route::post('/registration', [AuthController::class, 'create']);
 
 
 
 
 
 
-//Users
 
-Route::middleware(['auth', 'checkUserRole'])->group(function () {
-    Route::get('/employee', [ListController::class, 'employee']);
-    Route::get('/student', [ListController::class, 'student']);
-    Route::get('/student/search', [ListController::class, 'search']);
-    Route::get('/employee/search', [ListController::class, 'search']);
-    Route::post('/employee/create', [ListController::class, 'create']);
-    Route::post('/student/create', [ListController::class, 'create_stud']);
-    Route::put('/employee/update/{id}',[ListController::class,'update']);
-    Route::put('/student/update/{id}',[ListController::class,'update_stud']);
-    Route::delete('/employee/delete/{id}',[ListController::class,'delete']);
-    Route::delete('/student/delete/{id}',[ListController::class,'delete_stud']);
-    });
+
+// Route::middleware(['auth', 'checkUserRole'])->group(function () {
+//     Route::get('/employee', [ListController::class, 'employee']);
+//     Route::get('/student', [ListController::class, 'student']);
+//     Route::get('/student/search', [ListController::class, 'search']);
+//     Route::get('/employee/search', [ListController::class, 'search']);
+//     Route::post('/employee/create', [ListController::class, 'create']);
+//     Route::post('/student/create', [ListController::class, 'create_stud']);
+//     Route::put('/employee/update/{id}',[ListController::class,'update']);
+//     Route::put('/student/update/{id}',[ListController::class,'update_stud']);
+//     Route::delete('/employee/delete/{id}',[ListController::class,'delete']);
+//     Route::delete('/student/delete/{id}',[ListController::class,'delete_stud']);
+    
+//     });
+   
+
+
+
+
+    //Users
+    
+    Route::post('/create', [UserController::class,'create']);
+    Route::post('/login', [UserController::class,'login']);
+    Route::put('/update/{id}', [UserController::class,'update']);
+    Route::delete('/delete/{id}', [UserController::class,'delete']);
+    Route::get('/show', [UserController::class,'show']);
